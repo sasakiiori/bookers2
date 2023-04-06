2ae4e699b023457dd976dbeed808163340ecd258
sasakiiori <126736985+sasakiiori@users.noreply.github.com>
2023-04-06 12:38:53 +0000

    initial commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, uniqueness: true, length: { in: 2..20 }
+  validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
 
   has_many :books, dependent: :destroy
