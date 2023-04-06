6c96821b3880d94ee4ecce08307b29db156005bd
sasakiiori <126736985+sasakiiori@users.noreply.github.com>
2023-04-06 07:55:16 +0000

    initial commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -42,6 +42,12 @@ class BooksController < ApplicationController
     redirect_to '/books'
   end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
